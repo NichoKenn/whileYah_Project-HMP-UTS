5fe834a18d6b3bcbcf35b5c4a702a7401dd785ef
BaksoMalang1945 <hansstephen03@gmail.com>
2026-09-21 16:20:35 +0700

    Setup Routing+Setup dashboard
    Melakukan routing ke masing-masing page
    melakukan codingan untuk menampilkan dashboard awal

--- whileYah/src/app/app-routing.module.ts
+++ whileYah/src/app/app-routing.module.ts
@@ -3,21 +3,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },  {
     path: 'dashboard',
     loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardPageModule)
   },
   {
     path: 'produk',
-    loadChildren: () => import('./produk/produk.module').then( m => m.ProdukPageModule)
-  },
+    loadChildren: () => import('./produk/produk.module').then( m => m.ProdukPageModule) },
   {
     path: 'transaksi',
     loadChildren: () => import('./transaksi/transaksi.module').then( m => m.TransaksiPageModule)
@@ -38,7 +34,10 @@ const routes: Routes = [
     path: 'logout',
     loadChildren: () => import('./logout/logout.module').then( m => m.LogoutPageModule)
   },
-
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  }
 ];
 
 @NgModule({
